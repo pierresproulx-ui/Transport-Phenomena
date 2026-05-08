8b026fdc7d0c114d412013beb47c780afc5d2628
Pierre Proulx <pierre.s.proulx@gmail.com>
2026-05-08 14:12:15 -0400

    added some exercices from chap 8

---
+++ solutions/pheno.py
@@ -0,0 +1,11 @@
+from IPython.display import display, Math
+import sympy as sp
+sp.init_printing(use_latex=True)
+import matplotlib.pyplot as plt
+#%matplotlib inline
+from ipywidgets import *
+import thermo as th
+import fluids as fl
+from fluids.units import *
+import ht as ht
+
